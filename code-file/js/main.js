6150d61b4ee039121d17d61f7f0199b238d2bbca
coder-fokhrul <85823229+coder-fokhrul@users.noreply.github.com>
2021-06-13 14:53:27 +0600

    first projects

---
+++ code-file/js/main.js
@@ -0,0 +1,25 @@
+// Menu Button Design
+
+function openMenu() {
+    document.getElementById('nevbar').style.width="100%";
+}
+
+function closeMenu() {
+    document.getElementById('nevbar').style.width="0%";
+}
+
+//Smooth Scroll
+
+$('.menu-list a, scroll-button a').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+    return false
+});
+
+
+//WOW Plugin
+new WOW().init();
+
+//MixitUp
+var mixer = mixitup('.grid-gallery');
